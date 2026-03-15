9f1ede3c4f5b6717b74bfc93ff544f2fc9367fda
Eman-Mohamed-Hamed <122156132+Eman-Mohamed-Hamed@users.noreply.github.com>
2026-03-16 00:34:24 +0200

    Create colors.js

---
+++ utils/colors.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+  primary: "#0066ff",
+  secondary: "#00ff88",
+  danger: "#ff4444",
+  background: "#0a0e27",
+  card: "#162447",
+  textPrimary: "#ffffff",
+  textSecondary: "#8892b0"
+};
